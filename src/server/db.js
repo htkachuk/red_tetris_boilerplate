7722a3d55ca0c13197b146e4b4963fbe2302ee9c
kstasi <nastasiakondaurova@gmail.com>
2020-01-29 19:41:08 +0200

    async/await supported

--- src/server/db.js
+++ src/server/db.js
@@ -1,57 +1,68 @@
 const MongoClient = require('mongodb').MongoClient;
 const url = "mongodb://localhost:27017";
+let client = new MongoClient(url, {useNewUrlParser: true});
+const dbName = "red_tetris";
+let db;
+client.connect().then((client) => {
+    db = client.db(dbName);
+})
 
-class DatabaseInstance {
-    client;
-    db;
-    async constructor(){
-        this.client = new MongoClient(url, {useNewUrlParser: true});
-        const dbName = "red_tetris";
-        await client.connect();
-        this.db = client.db(dbName);
-    }
 
-    async getUser(req, res) {
-        let users = this.db.collection("users");
+module.exports.getUser = async(req, res) =>  {
+        let users = db.collection("users");
         let result = await users.findOne({login: req.body.login});
         res.json({ result: result});
-    
+        return res;
     }
 
-    async createUser(req, res) {
-        let users = this.db.collection("users");
-        if (await users.findOne({login: req.body.login}) !== None) {
-            res.json({ error: "user exists"});
+module.exports.createUser = async(body) =>  {
+        let users = db.collection("users");
+        let existedUser = await users.findOne({login: body.login});
+        if (existedUser !== null) {
+            console.log(existedUser);
+            return JSON.stringify({ error: "user exists"});
         } else {
             let user = {
-                login: req.body.login,
-                password: req.body.password,
+                login: body.login,
+                password: body.password,
                 totalScore: 0
             };
             let result = await users.insertOne(user);
-            res.json({ result: result['ops']});
+            console.log(existedUser);
+            return JSON.stringify({ result: result['ops']});
         }
     }
     
-    async updateUser(req, res) {
-
+module.exports.updateUser = async(req, res) =>  {
+        let users = db.collection("users");
+        let user = await users.findOne({login: req.body.login});
+        if (user !== None) {
+            res.json({ error: "user exists"});
+        } else {
+            user.totalScore = req.body.totalScore;
+            result = await events.findOneAndUpdate({login: user.login}, {$set: user});
+            res.json({ result: result});
+        }
+        return res;
     }
 
-    async deleteUser(req, res) {
-        let users = this.db.collection("users");
+module.exports.deleteUser = async(req, res) =>  {
+        let users = db.collection("users");
         let result = await users.findOneAndDelete({login: req.body.login});
         res.json({ result: result});
+        return res;
     }
-    async getRoom(req, res) {
-        let rooms = this.db.collection("rooms");
+module.exports.getRoom = async(req, res) =>  {
+        let rooms = db.collection("rooms");
         let result = await rooms.findOne({name: req.body.name});
         res.json({ result: result});
+        return res;
     }
     
-    async createRoom(req, res) {
-        let rooms = this.db.collection("rooms");
+module.exports.createRoom = async(req, res) =>  {
+        let rooms = db.collection("rooms");
         if (await rooms.findOne({name: req.body.name}) !== None) {
-            res.json({ error: "user exists"});
+            res.json({ error: "user doesn't exists"});
         } else {
             let room = {
                 name: req.body.mame,
@@ -60,18 +71,25 @@ class DatabaseInstance {
             let result = await rooms.insertOne(room);
             res.json({ result: result['ops']});
         }
+        return res;
     }
     
-    async updateRoom(req, res) {}
+module.exports.updateRoom = async(req, res) =>  {
+        let rooms = db.collection("rooms");
+        let room = await rooms.findOne({name: req.body.name});
+        if (room !== None) {
+            res.json({ error: "room doesn't exists"});
+        } else {
+            room.participants = req.body.participants;
+            result = await events.findOneAndUpdate({name: req.body.name}, {$set: room});
+            res.json({ result: result});
+        }
+        return res;
+    }
 
-    async deleteRoom(req, res) {
-        let rooms = this.db.collection("rooms");
+module.exports.deleteRoom = async(req, res) => {
+        let rooms = db.collection("rooms");
         let result = await rooms.findOneAndDelete({name: req.body.name});
         res.json({ result: result});
+        return res;
     }
-}
-
-const databaseInstance = new DatabaseInstance();
-Object.freeze(databaseInstance);
-
-export default databaseInstance;
--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,6 @@
 import fs  from 'fs'
 import debug from 'debug'
+import * as databaseInstance from './db'
 
 const logerror = debug('tetris:error')
   , loginfo = debug('tetris:info')
@@ -40,7 +41,61 @@ const initEngine = io => {
 
 export function create(params){
   const promise = new Promise( (resolve, reject) => {
-    const app = require('http').createServer()
+    const app = require('http').createServer(
+      function (req, res) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+      
+        let url = req.url;
+        let body = '';
+        let response = '';
+
+        req.on('data', function (chunk) {
+            body += chunk;
+        }).on('end', async () => {
+          if (url ==='/user') {
+  
+            switch (req.method) {
+              case 'GET':
+                res = databaseInstance.getUser(req, res);
+                break;
+              case 'POST':
+                response = await databaseInstance.createUser(body);
+                break;
+              case 'PUT':
+                res = databaseInstance.updateUser(req, res);
+                break;     
+              case 'DELETE':
+                res = databaseInstance.deleteUser(req, res);
+                break;       
+              default:
+                break;
+              }
+          } else if(url ==='/room') {
+          switch (req.method) {
+            case 'GET':
+              res = databaseInstance.getRoom(req, res);
+              break;
+            case 'POST':
+              res = databaseInstance.createRoom(req, res);
+              break;
+            case 'PUT':
+              res = databaseInstance.updateRoom(req, res);
+              break;     
+            case 'DELETE':
+              res =  databaseInstance.deleteRoom(req, res);
+              break;       
+            default:
+              break;
+            }
+      // res.end(); 
+          } else {
+            res.write('<h1>Hello World!<h1>');
+            res.end(); 
+          }
+          console.log(response);
+          res.end(response);
+        })
+    });
     initApp(app, params, () =>{
       const io = require('socket.io')(app)
       const stop = (cb) => {
