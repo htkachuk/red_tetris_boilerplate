ac51d0df8d1441fdd47a2c96486988a3a55055cb
kstasi <nastasiakondaurova@gmail.com>
2020-02-01 10:03:59 +0200

    register page added

--- src/client/components/RegisterPage.js
+++ src/client/components/RegisterPage.js
@@ -2,21 +2,48 @@ import React from "react";
 import { push } from "connected-react-router";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
+import Button from "react-bootstrap/Button";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
 
 const RegisterPage = props => (
   <div>
-    <h1>RegisterPage</h1>
-    <p>Welcome RegisterPage!</p>
-    <button onClick={() => props.changePage()}>
-      Go to about page via redux
-    </button>
+    <div className="txt-secondary">Sign In</div>
+    <Form.Group controlId="loginForm">
+      <Form.Control className="input-std" type="email" placeholder="Login" />
+      <Form.Control
+        className="input-std"
+        type="password"
+        placeholder="Password"
+      />
+      <Form.Control
+        className="input-std"
+        type="password"
+        placeholder="Repeat Password"
+      />
+    </Form.Group>
+
+    <Nav.Link
+      className="txt-link mt-0"
+      href="#"
+      onClick={() => props.changePage("/login")}
+    >
+      Have an account?
+    </Nav.Link>
+    <Button
+      variant="danger"
+      className="btn-std"
+      onClick={() => props.changePage("/lobby")}
+    >
+      Sign In
+    </Button>
   </div>
 );
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
-      changePage: () => push("/register")
+      changePage: page => push(page)
     },
     dispatch
   );
