800cbb4a0d8627cf9e569dfd7ce75890d9640fdf
htkachuk <annakristina50@gmail.com>
2020-02-01 23:12:00 +0200

    validation moved to helpers

--- src/server/db.js
+++ src/server/db.js
@@ -12,9 +12,10 @@ client.connect().then(client => {
 module.exports.createUser = async action => {
   const users = db.collection("users");
   const existedUser = await users.findOne({ login: action.login });
-  if (existedUser !== null) {
-    return JSON.stringify({ error: "user exists" });
-  } else {
+  const validationResult = await helpers.createUserValidation(
+    existedUser
+  );
+  if (validationResult.result !== "error") return validationResult;
     let user = {
       login: action.login,
       password: action.password,
@@ -30,45 +31,40 @@ module.exports.createUser = async action => {
 module.exports.createRoom = async action => {
   const rooms = db.collection("rooms");
   const existedRoom = await rooms.findOne({ name: action.name });
-  if (existedRoom !== null) {
-    return JSON.stringify({ error: "Room exists" });
-  } else {
-    const users = db.collection("users");
-    const currentUser = await users.findOne({ connection: action.id });
-    if (currentUser === null)
-      return JSON.stringify({ error: "user doesn't exists" });
-    if (currentUser.roomName !== null)
-      return JSON.stringify({ error: "user has room" });
-    currentUser.room = action.name;
-    const room = {
-      name: action.name,
-      participants: [currentUser.login],
-      leader: currentUser.login
-    };
-    const resultRoom = await rooms.insertOne(room);
-    await users.findOneAndUpdate(
-      { connection: action.id },
-      { $set: currentUser }
-    );
-    return JSON.stringify({ result: resultRoom["ops"] });
-  }
+  const users = db.collection("users");
+  const currentUser = await users.findOne({ connection: action.id });
+  const validationResult = await helpers.createRoomValidation(
+    existedRoom,
+    currentUser
+  );
+  if (validationResult.result !== "error") return validationResult;
+
+  currentUser.room = action.name;
+  const room = {
+    name: action.name,
+    participants: [currentUser.login],
+    leader: currentUser.login
+  };
+  const resultRoom = await rooms.insertOne(room);
+  await users.findOneAndUpdate(
+    { connection: action.id },
+    { $set: currentUser }
+  );
+  return JSON.stringify({ result: resultRoom["ops"] });
 };
 
 module.exports.loginUser = async action => {
   const users = db.collection("users");
   const user = await users.findOne({ login: action.login });
-  if (user === null) {
-    return JSON.stringify({ error: "user not exists" });
-  } else if (user.password !== action.password) {
-    return JSON.stringify({ error: "wrong password" });
-  } else {
-    user.connection = action.id;
-    const result = await users.findOneAndUpdate(
-      { login: action.login },
-      { $set: user }
-    );
-    return JSON.stringify({ result });
-  }
+  const validationResult = await helpers.loginValidation(user, action.password);
+
+  if (validationResult.result !== "error") return validationResult;
+  user.connection = action.id;
+  const result = await users.findOneAndUpdate(
+    { login: action.login },
+    { $set: user }
+  );
+  return JSON.stringify({ result });
 };
 
 module.exports.joinRoom = async action => {
@@ -76,12 +72,10 @@ module.exports.joinRoom = async action => {
   const existedRoom = await rooms.findOne({ name: action.name });
   const users = db.collection("users");
   const currentUser = await users.findOne({ connection: action.id });
-
   const validationResult = await helpers.joinRoomValidation(
     existedRoom,
     currentUser
   );
-  console.log(existedRoom);
   if (validationResult.result !== "error") return validationResult;
 
   existedRoom.participants.push(currentUser.login);
--- src/server/helpers.js
+++ src/server/helpers.js
@@ -19,3 +19,34 @@ module.exports.joinRoomValidation = async (existedRoom, currentUser) => {
   }
   return JSON.stringify({ result: "ok" });
 };
+
+module.exports.loginValidation = async (currentUser, password) => {
+  if (currentUser === null) {
+    return JSON.stringify({ error: "user not exists", result: "error" });
+  }
+  if (currentUser.password !== password) {
+    return JSON.stringify({ error: "wrong password", result: "error" });
+  }
+  return JSON.stringify({ result: "ok" });
+};
+
+module.exports.createRoomValidation = async (existedRoom, currentUser) => {
+  if (existedRoom !== null) {
+    return JSON.stringify({ error: "Room exists", result: "error" });
+  }
+  if (currentUser === null) {
+    return JSON.stringify({ error: "user doesn't exists", result: "error" });
+  }
+  if (currentUser.roomName !== null) {
+    return JSON.stringify({ error: "user has room", result: "error" });
+  }
+
+  return JSON.stringify({ result: "ok" });
+};
+
+module.exports.createUserValidation = async existedUser => {
+  if (existedUser !== null) {
+    return JSON.stringify({ error: "user exists", result: "error" });
+  }
+  return JSON.stringify({ result: "ok" });
+};
