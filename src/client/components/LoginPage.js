d2709488a61e3714fcd7955a46d10eb5850a17de
kstasi <nastasiakondaurova@gmail.com>
2020-02-05 21:57:10 +0200

    register and login actions dispatched

--- src/client/components/LoginPage.js
+++ src/client/components/LoginPage.js
@@ -5,40 +5,77 @@ import { connect } from "react-redux";
 import Button from "react-bootstrap/Button";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
+import { login } from "../actions/login";
 
-const LoginPage = props => (
-  <div>
-    <div className="txt-secondary">Sign Up</div>
-    <Form.Group controlId="loginForm">
-      <Form.Control className="input-std" type="email" placeholder="Login" />
-      <Form.Control
-        className="input-std"
-        type="password"
-        placeholder="Password"
-      />
-    </Form.Group>
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-    <Nav.Link
-      className="txt-link mt-7"
-      href="#"
-      onClick={() => props.changePage("/register")}
-    >
-      Not registered yet?
-    </Nav.Link>
-    <Button
-      variant="danger"
-      className="btn-std"
-      onClick={() => props.changePage("/lobby")}
-    >
-      Sign Up
-    </Button>
-  </div>
-);
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="txt-secondary">Sign Up</div>
+        <Form.Group controlId="loginForm">
+          <Form.Control
+            className="input-std"
+            type="email"
+            placeholder="Login"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
+          <Form.Control
+            className="input-std"
+            type="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+        </Form.Group>
+
+        <Nav.Link
+          className="txt-link mt-7"
+          href="#"
+          onClick={() => this.props.changePage("/register")}
+        >
+          Not registered yet?
+        </Nav.Link>
+        <Button
+          variant="danger"
+          className="btn-std"
+          onClick={() =>
+            this.props.login(this.state.email, this.state.password)
+          }
+        >
+          Sign Up
+        </Button>
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
-      changePage: page => push(page)
+      changePage: page => push(page),
+      login: (email, password) => {
+        dispatch(login(email, password));
+        return push("/lobby");
+      }
     },
     dispatch
   );
--- src/client/components/RegisterPage.js
+++ src/client/components/RegisterPage.js
@@ -7,46 +7,78 @@ import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import { register } from "../actions/register";
 
-const RegisterPage = props => (
-  <div>
-    <div className="txt-secondary">Sign In</div>
-    <Form.Group controlId="registerForm">
-      <Form.Control className="input-std" type="email" placeholder="Login" />
-      <Form.Control
-        className="input-std"
-        type="password"
-        placeholder="Password"
-      />
-      <Form.Control
-        className="input-std"
-        type="password"
-        placeholder="Repeat Password"
-      />
-    </Form.Group>
+class RegisterPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
 
-    <Nav.Link
-      className="txt-link mt-0"
-      href="#"
-      onClick={() => props.changePage("/login")}
-    >
-      Have an account?
-    </Nav.Link>
-    <Button
-      variant="danger"
-      className="btn-std"
-      onClick={() => props.register()}
-    >
-      Sign In
-    </Button>
-  </div>
-);
+  handleEmailChange(e) {
+    this.setState({ email: e.target.value });
+  }
+
+  handlePasswordChange(e) {
+    this.setState({ password: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="txt-secondary">Sign In</div>
+        <Form.Group controlId="registerForm">
+          <Form.Control
+            className="input-std"
+            type="email"
+            placeholder="Login"
+            value={this.state.email}
+            onChange={this.handleEmailChange}
+          />
+          <Form.Control
+            className="input-std"
+            type="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={this.handlePasswordChange}
+          />
+          <Form.Control
+            className="input-std"
+            type="password"
+            placeholder="Repeat Password"
+          />
+        </Form.Group>
+
+        <Nav.Link
+          className="txt-link mt-0"
+          href="#"
+          onClick={() => this.props.changePage("/login")}
+        >
+          Have an account?
+        </Nav.Link>
+        <Button
+          variant="danger"
+          className="btn-std"
+          onClick={() =>
+            this.props.register(this.state.email, this.state.password)
+          }
+        >
+          Sign In
+        </Button>
+      </div>
+    );
+  }
+}
 
 const mapDispatchToProps = dispatch =>
   bindActionCreators(
     {
       changePage: page => push(page),
-      register: () => {
-        dispatch(register("user123weооw", "password"));
+      register: (email, password) => {
+        dispatch(register(email, password));
         return push("/lobby");
       }
     },
