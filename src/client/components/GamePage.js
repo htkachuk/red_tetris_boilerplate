02847734c7cb02ebcbb7e9b248ea7ce8ed164515
kstasi <nastasiakondaurova@gmail.com>
2020-02-09 15:13:54 +0200

    Game layout pretified

--- src/client/components/GamePage.js
+++ src/client/components/GamePage.js
@@ -22,8 +22,14 @@ class GamePage extends React.Component {
           : rows[rows.length - 1].push(key)) && rows,
       []
     );
-    console.log(elementsGrid);
-
+    let contenders = Array.from(Array(6).keys());
+    let contendersGrid = contenders.reduce(
+      (rows, key, index) =>
+        (index % 3 == 0 ? rows.push([key]) : rows[rows.length - 1].push(key)) &&
+        rows,
+      []
+    );
+    console.log(contendersGrid);
     return (
       <div className="h-100 w-100">
         <form
@@ -40,7 +46,7 @@ class GamePage extends React.Component {
             <Row>
               <Col className="game-frame mx-5" id="game-field" sm={5}>
                 {elementsGrid.map((elements, index) => (
-                  <Row>
+                  <Row key={index}>
                     {elements.map((unit, index) => (
                       <Col className="game-unit" key={index}>
                         {unit}
@@ -51,11 +57,31 @@ class GamePage extends React.Component {
               </Col>
               <Col sm={5}>
                 <Row>
-                  <Col className="game-frame mx-3 mb-4">1</Col>
-                  <Col className="game-frame mx-3 mb-4">1</Col>
+                  <Col className="game-frame game-box mx-3 mb-4"></Col>
+                  <Col className="game-frame game-box mx-3 mb-4 text-left">
+                    {this.props.room.participants ? (
+                      this.props.room.participants.map((player, index) => (
+                        <Row>
+                          <Col key={index}>{player}</Col>
+                        </Row>
+                      ))
+                    ) : (
+                      <div></div>
+                    )}
+                  </Col>
                 </Row>
-                <Row>
-                  <Col className="game-frame mx-3">1</Col>
+                <Row id="other-fields">
+                  <Col className="game-frame mx-3">
+                    {contendersGrid.map((contenders, index) => (
+                      <Row key={index}>
+                        {contenders.map((unit, index) => (
+                          <Col className="other-field" key={index}>
+                            {unit}
+                          </Col>
+                        ))}
+                      </Row>
+                    ))}
+                  </Col>
                 </Row>
               </Col>
             </Row>
