0e11dea8dd80deaafc5810eea390f9c5a8561584
htkachuk <annakristina50@gmail.com>
2020-02-09 14:30:29 +0200

    Piece class

--- src/server/models/Piece.js
+++ src/server/models/Piece.js
@@ -1,19 +1,40 @@
 class Piece {
   constructor(shapes, color) {
-    this.shape = shapes;
+    this.shapes = shapes;
     this.color = color;
   }
   shape() {
-    return this.shape[0];
+    return this.shapes[0];
   }
   rotateLeft() {
-    this.shape.push(this.shape.shift());
+    this.shapes.push(this.shapes.shift());
   }
+  width() {
+    return this.shapes[0].indexOf("|");
+  }
+  height() {
+    return this.shapes[0].split("|").length;
+  }
+  getCell(x, y) {
+    const currentPiece = this.shapes[0];
 
-  // TODO:
-  width() {}
-  height() {}
-  getCell() {}
+    if (
+      x < 0 ||
+      y < 0 ||
+      x >= currentPiece.width() ||
+      y >= currentPiece.height()
+    ) {
+      return false;
+    }
+
+    const pos = currentPiece.split("|")[y].substr(x, 1);
+
+    if (pos === "1") {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 
 export default Piece;
