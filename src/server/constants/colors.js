4627efae4072678fb6bc4e7b1fed88d1b0d953b8
htkachuk <annakristina50@gmail.com>
2020-02-10 20:35:01 +0200

    constants update

---
+++ src/server/constants/colors.js
@@ -0,0 +1,9 @@
+const RED = "F91E08";
+const GREEN = "3FF908";
+const BLUE = "08F9EB";
+const DARK_BLUE = "1708F9";
+const YELLOW = "F9EE08";
+const ORANGE = "F99708";
+const VIOLET = "A908F9";
+
+export const colors = [RED, GREEN, BLUE, DARK_BLUE, YELLOW, ORANGE, VIOLET];
