f47946afe74daf0e98693dabf052de981222fa7e
htkachuk <annakristina50@gmail.com>
2020-02-10 20:35:45 +0200

    models update

---
+++ src/server/models/Game.js
@@ -0,0 +1,31 @@
+import { pieces } from "../constants/pieces";
+import Piece from "./Piece";
+
+class Game {
+  constructor(mode, players) {
+    this.mode = mode;
+    this.players = players;
+    this.endGame = false;
+    this.startGame = false;
+    this.speed = 0;
+  }
+
+  getPiece() {
+    const index = Math.floor(Math.random() * Math.floor(pieces.length));
+    return Piece(pieces[index], color[index]);
+  }
+
+  addSpeed() {
+    this.speed += 10;
+  }
+
+  startGame() {
+    this.startGame = true;
+  }
+
+  endGame() {
+    this.endGame = true;
+  }
+}
+
+export default Game;
---
+++ src/server/models/Player.js
@@ -0,0 +1,9 @@
+class Player {
+  constructor(currentGame) {
+    this.game = currentGame;
+    this.score = 0;
+    this.board;
+  }
+}
+
+export default Player;
