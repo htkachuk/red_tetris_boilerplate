f1eaeca5864d5c8c3a917ea5dd2bea3013e845c8
htkachuk <annakristina50@gmail.com>
2020-02-12 22:16:22 +0200

    movements for pieces

--- src/server/models/Piece.js
+++ src/server/models/Piece.js
@@ -1,39 +1,11 @@
 class Piece {
-  constructor(shapes, color) {
-    this.shapes = shapes;
-    this.color = color;
+  rotateLeft(piece) {
+    return piece.push(piece.shift());
   }
-  shape() {
-    return this.shapes[0];
-  }
-  rotateLeft() {
-    this.shapes.push(this.shapes.shift());
-  }
-  width() {
-    return this.shapes[0].indexOf("|");
-  }
-  height() {
-    return this.shapes[0].split("|").length;
-  }
-  getCell(x, y) {
-    const currentPiece = this.shapes[0];
 
-    if (
-      x < 0 ||
-      y < 0 ||
-      x >= currentPiece.width() ||
-      y >= currentPiece.height()
-    ) {
-      return false;
-    }
-
-    const pos = currentPiece.split("|")[y].substr(x, 1);
-
-    if (pos === "1") {
-      return true;
-    } else {
-      return false;
-    }
+  rotateRight(piece) {
+    let oldPiece = piece;
+    return piece.unshift(oldPiece.splice(2, 1));
   }
 }
 
