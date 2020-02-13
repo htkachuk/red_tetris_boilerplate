acd7ad1fac06986a03e06fe5aa276719e0bb2b2f
htkachuk <annakristina50@gmail.com>
2020-02-13 22:05:36 +0200

    random piece

--- src/server/models/Piece.js
+++ src/server/models/Piece.js
@@ -7,6 +7,19 @@ class Piece {
     let oldPiece = piece;
     return piece.unshift(oldPiece.splice(2, 1));
   }
+
+  getRandomPiece() {
+    const index = Math.floor(Math.random() * Math.floor(pieces.length));
+
+    const Piece = {
+      color: colors[index],
+      x: 3,
+      y: -1,
+      piece: pieces[index],
+      index
+    };
+    return Piece;
+  }
 }
 
 export default Piece;
