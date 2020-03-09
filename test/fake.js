0e7c1c0b67d6f5750f20a2f71bb8d8c884b107c6
Hanna TKACHUK <htkachuk@e1r6p11.unit.ua>
2020-03-09 18:02:32 +0200

    moving piece test ready

--- test/fake.js
+++ test/fake.js
@@ -1,11 +1,71 @@
 import chai from "chai"
+import Player from "../src/server/models/Player"
+import Piece from "../src/server/models/Piece"
+import Board from "../src/server/models/Board"
 
 chai.should()
 
-describe('Check Sum', () => {
-  it('1+1 == 2', () => {
-    const res = 1 + 1
-    res.should.equal(2)
+describe('Check newBoard', () => {
+  it('Get emty board', () => {
+    const res = Player.newBoard();
+    const board = [ [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ] ]
+    res.should.to.deep.equal(board)
   });
+});
 
+describe('check movePiece function', () => {
+  it('Get Piece move', () => {
+    const board = Player.newBoard();
+    const piece = new Piece(0);
+    const boardObj = new Board();
+    const res = boardObj.moveBottom(board, piece, 20);
+    const boardWithPiece =    
+    [ [ 0, 0, 0, 'F91E08', 'F91E08', 'F91E08', 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 'F91E08', 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ] ];
+    res.board.should.to.deep.equal(boardWithPiece)
+    const rightRotation = boardObj.rotateRight(boardWithPiece, piece);
+    const leftRotation = boardObj.rotateLeft(rightRotation.board, rightRotation.piece)
+    leftRotation.board.should.to.deep.equal(boardWithPiece)
+    const leftMoving = boardObj.moveLeft(boardWithPiece, piece);
+    const rightMoving = boardObj.moveRight(leftMoving.board, leftMoving.piece);
+    rightMoving.board.should.to.deep.equal(boardWithPiece)
+
+  });
 });
